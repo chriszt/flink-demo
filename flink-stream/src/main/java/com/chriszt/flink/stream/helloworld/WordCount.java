1fb7ad6811f14f1948b349fc47a61ec3104b5158
YanLin <chriszt@126.com>
2021-08-29 19:14:13 +0800

    mod log4j2.xml to flink-stream

--- flink-stream/src/main/java/com/chriszt/flink/stream/helloworld/WordCount.java
+++ flink-stream/src/main/java/com/chriszt/flink/stream/helloworld/WordCount.java
@@ -7,8 +7,13 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class WordCount {
+
+    private static final Logger log = LogManager.getLogger();
 
     public void wordCount(String[] words) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -31,6 +36,7 @@ public class WordCount {
         }).sum(1);
 
         ds2.print();
+        log.info("*************");
         try {
             env.execute();
         } catch (Exception e) {
